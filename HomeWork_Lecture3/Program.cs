0b22e2777fe85ad177ba4d32be10d6674168a0f9
Wampirs <79167501+Wampirs@users.noreply.github.com>
2022-07-12 11:48:13 +0300

    Implemented factorial calculator

--- HomeWork_Lecture3/Program.cs
+++ HomeWork_Lecture3/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace HomeWork_Lecture3
+﻿using HomeWork_Lecture3.Calculators;
+
+namespace HomeWork_Lecture3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int fact = FactorialCalculator.CalculateFactorial(Int32.Parse(Console.ReadLine()));
+            Console.WriteLine($"Factorial of number is {fact}");
         }
     }
 }
