9c2074870a28209bcb58352d82d9e4289e6ea582
Wampirs <79167501+Wampirs@users.noreply.github.com>
2022-07-12 10:58:09 +0300

    Created solution

---
+++ HomeWork_Lecture3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HomeWork_Lecture3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
